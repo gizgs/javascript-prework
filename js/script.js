00da6c57e0b771b35822e326c6395b5418681fbc
Artur Giza <arthur.giza@gmail.com>
2020-05-31 22:36:27 +0200

    add playerMove

--- js/script.js
+++ js/script.js
@@ -10,8 +10,35 @@ if(randomNumber == 1){
  else if(randomNumber == 2){
   computerMove = 'papier';
 }
+ else if(randomNumber == 3){
+  computerMove = 'nożyczki';
+  }
  else {
-  computerMove = 'nożyczki'; 
+  computerMove = 'nieznany ruch'; 
  }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove;
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+ else if(playerInput == 2){
+	playerMove = 'papier';
+}
+ else if(playerInput == 3){
+	playerMove = 'nożyczki';
+}
+ else {
+	playerMove = 'nieznany ruch'; 
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+let score;
+
