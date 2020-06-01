d9f64f8fd4387ce4db6aacb49727d27bab195d2d
Artur Giza <arthur.giza@gmail.com>
2020-06-01 21:32:29 +0200

    end second task

--- js/script.js
+++ js/script.js
@@ -1,7 +1,80 @@
-let computerMove = `kamień`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-let playerMove = `papier`;
+let computerMove;
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+ else if(randomNumber == 2){
+  computerMove = 'papier';
+}
+ else if(randomNumber == 3){
+  computerMove = 'nożyczki';
+  }
+ else {
+  computerMove = 'nieznany ruch'; 
+ }
+
+printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove;
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+ else if(playerInput == 2){
+	playerMove = 'papier';
+}
+ else if(playerInput == 3){
+	playerMove = 'nożyczki';
+}
+ else {
+	playerMove = 'nieznany ruch'; 
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+let score;
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+ else if (computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Jest remis');
+}
+ else if (computerMove == 'kamień' && playerMove == 'nożyczki'){
+  printMessage('Ty przegrywasz');
+}
+ else if( computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+  printMessage('Wybierz odpowiednią cyfrę');
+}
+ else if (computerMove == 'nożyczki' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz');
+}
+ else if (computerMove == 'nożyczki' && playerMove == 'papier'){  
+  printMessage('Ja wygrywam');
+}
+ else if (computerMove == 'nożyczki' && playerMove == 'nożyczki'){
+  printMessage('Jest remis');
+}
+ else if( computerMove == 'nożyczki' && playerMove == 'nieznany ruch'){
+  printMessage('Wybierz odpowiednią cyfrę');
+}
+else if (computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Ty przegrywasz');
+}
+ else if (computerMove == 'papier' && playerMove == 'papier'){  
+  printMessage('Jest remis');
+}
+ else if (computerMove == 'papier' && playerMove == 'nożyczki'){
+  printMessage('Ty wygrywasz');
+}
+ else if( computerMove == 'papier' && playerMove == 'nieznany ruch'){
+  printMessage('Wybierz odpowiednią cyfrę');
+}
